1035fbcd8c7a369495b3fba94f3a4cf43e4c728a
Tiago Matos <tiago@tiagomatos.com>
2024-02-07 10:01:16 +1100

    Adicionado token na factory do usuário

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
         return [
             'first_name' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
+            'token' => fake()->uuid(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
