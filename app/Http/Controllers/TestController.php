6de467795c544febe1a576237767de942ace96de
Tiago Matos <tiago@tiagomatos.com>
2024-02-27 15:41:48 +1100

    Aprendendo a trabalhar com times parte 2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class TestController extends Controller
 {
     public function __invoke()
     {
-        dd('x');
+        $roleAdmin = Role::find(1);
+        $roleAtendente = Role::find(2);
+
+        // Criando permissões
+        // $permissionDeleteUser = Permission::create(['name' => 'delete user']);
+
+        // Atribuindo permissão ao cargo
+        // $roleAdmin->givePermissionTo($permissionDeleteUser);
+
+        setPermissionsTeamId(1);
+
+        // Pedro - dono da Dental Systems
+        $pedro = User::find(1);
+//        $pedro->assignRole($roleAdmin);
+        dd($pedro->can('delete user'));
+
+        // Melisa - Não é nada
+        $melisa = User::find(2);
+//        dd($melisa->can('delete user'));
     }
 }
