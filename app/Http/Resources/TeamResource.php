600f1316b4c6ec794f94f37c8b4ac3b34b763505
Tiago Matos <tiago@tiagomatos.com>
2024-04-22 10:45:05 +1000

    Adicionando cargo ao loop de teams

--- app/Http/Resources/TeamResource.php
+++ app/Http/Resources/TeamResource.php
@@ -14,9 +14,14 @@ class TeamResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        setPermissionsTeamId($this->id);
+        $user = auth()->user();
+        $user->unsetRelation('roles')->unsetRelation('permissions');
+
         return [
             'token' => $this->token,
             'name' => $this->name,
+            'roles' => RoleResource::collection($user->roles),
             'default' => auth()->user()->default_team_id === $this->id,
         ];
     }
